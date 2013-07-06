322f59b63a39aa45bb47c9b62f8e9a6a509f9629
Michelle Anderson <mimi@mimis-air.config>
2013-07-06 12:08:36 +0100

    Adding Grunt to project, and Travis .yml file for CI

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function (grunt) {
+
+  /*
+      Grunt installation:
+      -------------------
+          npm install -g grunt-cli
+          npm install -g grunt-init
+          npm init (creates a `package.json` file)
+
+      Project Dependencies:
+      ---------------------
+          npm install grunt --save-dev
+          npm install grunt-contrib-watch --save-dev
+          npm install grunt-contrib-jshint --save-dev
+          npm install grunt-contrib-sass --save-dev
+          */
+
+  // Project configuration.
+  grunt.initConfig({
+
+      // Store your Package file so you can reference its specific data whenever necessary
+      pkg: grunt.file.readJSON('package.json'),
+
+      sass: {
+        dev: {
+          options: {
+            style: 'expanded'
+          },
+          src: ['./app/assets/styles/sass/app.scss'],
+          dest: './app/assets/styles/css/app.css'
+        }
+      },
+
+      // Run: `grunt watch` from command line for this section to take effect
+      watch: {
+        //files: ['<%= jshint.files %>', '<%= sass.dev.src %>'],
+        files: ['<%= sass.dev.src %>'],
+        tasks: 'default'
+      }
+    });
+
+  // Load NPM Tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  //grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default Task
+  grunt.registerTask('default', ['sass:dev']);
+
+  // Unit Testing Task
+  //grunt.registerTask('test', ['connect', 'jasmine']);
+
+  // Release Task
+  //grunt.registerTask('release', ['jshint', 'sass:dist']);
+
+};
